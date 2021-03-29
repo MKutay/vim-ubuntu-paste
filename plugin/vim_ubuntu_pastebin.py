e6c1c25e32c72fbaaf642f985c821c8a94dbacb3
Mehmet Kutay Bozkurt <47640467+MKutay@users.noreply.github.com>
2021-03-29 17:05:26 +0300

    Update vim_ubuntu_pastebin.py

--- plugin/vim_ubuntu_pastebin.py
+++ plugin/vim_ubuntu_pastebin.py
@@ -240,14 +240,12 @@ def post_buffer_to_ubuntu_pastebin(buffer, filetype, open_in_borwser=True):
         'Upgrade-Insecure-Requests': '1',
         'Origin': 'https://paste.ubuntu.com',
         'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'navigate',
         'Sec-Fetch-User': '?1',
         'Sec-Fetch-Dest': 'document',
         'Referer': 'https://paste.ubuntu.com/',
-        'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
         'sec-gpc': '1',
     }
     r = requests.post(url, headers=headers, data=data, allow_redirects=True)
